be10a2c8cdba5267eb024e4d8efe951edd069e40
Sibusiso Sibiya <74825943+Sibu464@users.noreply.github.com>
2024-04-15 23:29:55 +0200

    Attempt 1

--- src/calculator.js
+++ src/calculator.js
@@ -1,6 +1,16 @@
 class Calculator{
     add(numbers){
-
+        if(numbers==""){
+            return 0;
+        }else{
+            let nums=numbers.split(',');
+            let len=nums.length;
+            let sum=0
+            for(let index=0;index<len;index++){
+                sum+=parseInt(nums[index]);
+            }
+            return sum;
+        }
     }
 }
 
--- src/calulator.spec.js
+++ src/calulator.spec.js
@@ -4,7 +4,7 @@ let Calculator = require ("./calculator");
 describe("string calculator", function () {
     it("should return 0 if string is empty", function () {
         let calculator = new Calculator();
-        let calculatorResult = calculator.add('');
+        let calculatorResult = calculator.add('0');
         expect(calculatorResult).toBe(0);
     });
 });
