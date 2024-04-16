52d88fd32965dd11bb671ab241beef79bfabfda4
Sibu464 <74825943+Sibu464@users.noreply.github.com>
2024-04-16 16:31:40 +0200

    add

--- src/calculator.js
+++ src/calculator.js
@@ -1,7 +1,53 @@
 class Calculator{
     add(numbers){
+       if(numbers==""){
+            return 0;
+       }else{
+        let numArr=['1','2','3','4','5','6','7','8','9','0'];
+        let delims=['\n',','];
+        // if(numArr.includes(numbers[0])){
+        //     console.log('runs');
+        //     let num=numbers.split(',');
+        //     let sum=0;
+        //     let len=num.length;
+        //     for(let index=0;index<len;index++){
+        //         sum+=parseInt(num[index]);
+        //     }
+            
+        //     return sum;
+        // }
+        var test=[];
+        var len=numbers.length;
+        console.log(`len: ${len}`);
+        numbers=numbers.replace(/\n/g,',');
+        for(let i=0;i<len;i++){
+         
+            if(numArr.includes(numbers[i])){
+              test.push(numbers[i]);
+               
+            }
+           
 
+        }
+ 
+        console.log(test);
+     
+        if(numbers.includes('\n')){
+         
+            let num=numbers.split(/[\n,]/);
+            let sum=0;
+            let len=num.length;
+          
+            for(let index=0;index<len;index++){
+                sum+=parseInt(num[index]);
+            }
+            return sum;
+        }
+        
+        
+       }
     }
+ 
 }
 
 module.exports = Calculator
